e5bd2c5b6d45710019409618e44b815a3a9fd8d3
BlueOrange <this.is.blueorange@gmail.com>
2020-03-20 12:55:12 +0545

    Additional checks added

--- main.sh
+++ main.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-set -e
 
 # colors defined
 RED='\033[0;31m'
@@ -8,40 +7,67 @@ GREEN='\033[0;32m'
 LCYAN='\033[1;36m'
 YELLOW='\033[0;33m'
 
+# check Node
+function checkNode(){
+    node -v &> /dev/null
+}
+
 # Checks for existing eslintrc file
 function checkEslint(){
     if [ -f ".eslintrc.js" -o -f ".eslintrc.yaml" -o -f ".eslintrc.yml" -o -f ".eslintrc.json" -o -f ".eslintrc" ]; then
-        echo -e "${RED}Existing ESLint config file(s) found:${NC}"
         ls -a .eslint* | xargs -n 1 basename
         echo
-        echo -e "${RED}CAUTION:${NC} there is loading priority when more than one config file is present: https://eslint.org/docs/user-guide/configuring#configuration-file-formats"
-        echo
-        echo -e "${RED}Improper Script Exit${NC}"
-        exit 1
+        return 1
     fi
 }
 
 function checkPrettier(){
     # Checks for existing prettierrc files
     if [ -f ".prettierrc.js" -o -f "prettier.config.js" -o -f ".prettierrc.yaml" -o -f ".prettierrc.yml" -o -f ".prettierrc.json" -o -f ".prettierrc.toml" -o -f ".prettierrc" ]; then
-        echo -e "${RED}Existing Prettier config file(s) found${NC}"
         ls -a | grep "prettier*" | xargs -n 1 basename
         echo
-        echo -e "${RED}Improper Script Exit${NC}"
-        exit 2
+        return 1
     fi
 }
 
+# check if node is installed
+if checkNode; then
+    echo
+    echo -e "${GREEN}Sweet! Node is available${NC}"
+else
+    echo
+    echo -e "${RED}Node isnt installed, Please install and try again${NC}"
+    exit 1
+fi
 
-checkEslint
-checkPrettier
+# check for eslint files
+if checkEslint; then
+    echo
+    echo -e "${GREEN}Working Directory seems clean! Marching Forward${NC}"
+else
+    echo
+    echo -e "${RED}Ancient eslint files found!${NC}"
+    exit 1
+fi
+
+# check for eslint files
+if checkPrettier; then
+    echo
+    echo -e "${GREEN}Working directory is pretty${NC}"
+else
+    echo
+    echo -e "${RED}Working directory is dirty!${NC}"
+    exit 1
+fi
 
 # ----------------------
 # Perform Configuration
 # ----------------------
 echo
 echo -e "${GREEN}Configuring your development environment... ${NC}"
-npm init -y
+if [ ! -f ./package.json ]; then
+    npm init -y &> /dev/null
+fi
 
 echo
 echo -e "1/5 ${LCYAN}ESLint & Prettier Installation... ${NC}"
